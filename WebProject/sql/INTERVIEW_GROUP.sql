b2c78f5c230c3afb71bffa61e4734d33cba3fce8
Scott-JH <scott@thekary.com>
2021-12-14 18:53:20 +0900

    정렬

--- WebProject/sql/INTERVIEW_GROUP.sql
+++ WebProject/sql/INTERVIEW_GROUP.sql
@@ -1,9 +1,9 @@
 create table INTERVIEW_GROUP
 (
-	interview_group_code varchar(30) not null,
-	interview_group_name varchar(60) not null,
-	create_date DATETIME default sysdate() not null,
-	update_date DATETIME default sysdate() not null,
+	interview_group_code varchar(30)                not null,
+	interview_group_name varchar(60)                not null,
+	create_date          DATETIME default sysdate() not null,
+	update_date          DATETIME default sysdate() not null,
 	constraint INTERVIEW_GROUP_pk
 		primary key (interview_group_code)
 );
